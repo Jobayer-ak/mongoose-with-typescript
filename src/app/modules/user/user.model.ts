58ad695f96a4eedf0d8082a020fa185ac2e60373
Jobayer_ak <jobayer.ak@gmail.com>
2023-06-16 21:06:47 +0600

    instance methods, build custom instance method

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,8 +1,10 @@
-import { Schema, model } from 'mongoose';
-import { IUser } from './user.interface';
+import { Model, Schema, model } from 'mongoose';
+import { IUser, IUserMethods } from './user.interface';
+
+type UserModel = Model<IUser, {}, IUserMethods>;
 
 // creating schema using interface
-const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
   id: {
     type: String,
     required: true,
@@ -53,6 +55,15 @@ const userSchema = new Schema<IUser>({
   permanentAddress: { type: String },
 });
 
-const User = model<IUser>('User', userSchema);
+userSchema.method("fullName", function fullName() {
+  return this.name.firstName + " " + this.name.lastName;
+})
+
+const User = model<IUser, UserModel>('User', userSchema);
 
 export default User;
+
+
+
+// isntance methods --> instance's methods
+// class -> instance + methods -> instance methods
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,13 +2,18 @@ import { IUser } from './user.interface';
 import User from './user.model';
 
 export const createUserToDB = async (payload:IUser): Promise<IUser> => {
-  const user = new User(payload);
-  await user.save();
+  const user = new User(payload); // User -> class user -> instance
+  await user.save(); //builtin instace methods .save() -> instance method // custom instance method
+
+  // while using post method we can use instance method
+  console.log(user.fullName());
   return user;
 };
 
 export const getUserFromDB = async(): Promise<IUser[]> => {
   const users = await User.find();
+
+  
   return users;
 }
 
@@ -16,3 +21,7 @@ export const getUserByIdFromDB = async (payload: string): Promise<IUser | null> 
   const user = await User.findOne({ id: payload }, {name: 1, email: 1});
   return user;
 }
+
+// export const getAllAdminUsers = async (payload: string): Promise<IUser | null> => {
+//   const user1 = new User() // static User
+// }
